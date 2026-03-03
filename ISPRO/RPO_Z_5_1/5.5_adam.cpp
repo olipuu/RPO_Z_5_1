68b75f33d3dc0501ea87d7a060e621926dd0c3d8
olipuu <famahyga@gmail.com>
2026-03-03 14:35:24 +0300

    6.3_adam.cpp
    Add to my repo 6.3_adam.cpp

--- ISPRO/RPO_Z_5_1/5.5_adam.cpp
+++ ISPRO/RPO_Z_5_1/5.5_adam.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <cmath>
-
-int main() {
-    system("chcp 1251");
-    int number;
-
-    std::cout << "¬ведите число: ";
-    std::cin >> number;
-
-    int absNumber = std::abs(number);
-
-    if (absNumber >= 100000 && absNumber <= 999999) {
-        std::cout << "¬ведЄнное число - шестизначное";
-    }
-    else {
-        std::cout << "¬ведЄнное число - не шестизначное";
-    }
-
-    return 0;
-}
+//#include <iostream>
+//#include <cmath>
+//
+//int main() {
+//    system("chcp 1251");
+//    int number;
+//
+//    std::cout << "¬ведите число: ";
+//    std::cin >> number;
+//
+//    int absNumber = std::abs(number);
+//
+//    if (absNumber >= 100000 && absNumber <= 999999) {
+//        std::cout << "¬ведЄнное число - шестизначное";
+//    }
+//    else {
+//        std::cout << "¬ведЄнное число - не шестизначное";
+//    }
+//
+//    return 0;
+//}
